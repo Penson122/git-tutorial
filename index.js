25ca6fbe17d5acfaa25404d8aa06c36bb4504273
Jack Penson <contact@jackpenson.dev>
2023-10-06 14:31:11 +0100

    serve bootstrap site

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || '8080'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!!!!')
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
