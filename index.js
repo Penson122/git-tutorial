27e7dc251ada1290ca5b3701f5860dab988d0f2e
Jack Penson <contact@jackpenson.dev>
2023-10-07 18:00:53 +0100

    add linter

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = process.env.PORT || '8080'
+const express = require('express');
+const path = require('path');
 
-app.get('/', function(req, res) {
+const app = express();
+const port = process.env.PORT || '8080';
+
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
